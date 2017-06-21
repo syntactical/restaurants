5e380bb189447a4bc28a4950c8062ebe59e6f8fe
Greg Dutcher <gdutcher@thoughtworks.com>
2017-06-21 08:22:06 -0400

    etl working

--- restaurants/data/etl.py
+++ restaurants/data/etl.py
@@ -5,24 +5,24 @@ from datetime import datetime
 # remove rows with no grade date?
 
 restaurants = (
-	etl
-	.fromcsv('i.csv')
-	.rename({'CAMIS': 'id', 'DBA': 'name'})
-	.cut("id", "name", "BORO", "BUILDING", "STREET", "ZIPCODE", "PHONE", "CUISINE DESCRIPTION", "GRADE", "GRADE DATE")
-	.selectisnot("GRADE", u"")
-	.select("CUISINE DESCRIPTION",lambda cuisine: 'thai' in cuisine.lower())
-	.cutout("CUISINE DESCRIPTION")
-	.convert("GRADE DATE", lambda date: datetime.strptime(date, '%m/%d/%Y'))
-	.groupselectmax("name", "GRADE DATE")
-	.cutout("GRADE DATE")
-	.select("GRADE", lambda grade: grade.lower() <= u"b")
-	.addfield('address',lambda table: table['BUILDING'] + ' ' + table['STREET'])
-	.cutout("BUILDING","STREET")
+    etl
+    .fromcsv('inspections.csv')
+    .rename({'CAMIS': 'id', 'DBA': 'name', 'BORO': 'city', 'ZIPCODE': 'zip_code', 'PHONE': 'phone', 'GRADE': 'grade'})
+    .cut("id", "name", "city", "BUILDING", "STREET", "zip_code", "phone", "CUISINE DESCRIPTION", "grade", "GRADE DATE")
+    .selectisnot("grade", u"")
+    .select("CUISINE DESCRIPTION",lambda cuisine: 'thai' in cuisine.lower())
+    .cutout("CUISINE DESCRIPTION")
+    .convert("GRADE DATE", lambda date: datetime.strptime(date, '%m/%d/%Y'))
+    .groupselectmax("name", "GRADE DATE")
+    .cutout("GRADE DATE")
+    .select("grade", lambda grade: grade.lower() <= u"b")
+    .addfield('address',lambda table: table['BUILDING'] + ' ' + table['STREET'])
+    .cutout("BUILDING","STREET")
 )
 
 etl.tocsv(restaurants, 'cleaned_inspections.csv')
 
-db_connection = psycopg2.connect('dbname=example user=postgres')
-etl.todb(restaurants, db_connection, 'restaurants')
+conn = psycopg2.connect('user=postgres password=postgres')
+etl.todb(restaurants, conn, 'restaurants')
 
 
