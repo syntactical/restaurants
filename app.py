480a5ee0628225982965e49495d2f6e4644a8806
Greg Dutcher <gdutcher@thoughtworks.com>
2017-06-21 23:40:31 -0400

    remove .env

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from models.restaurant import Restaurant, db
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost/restaurants'
+
 db.init_app(app)
 with app.app_context():
         db.create_all()
+
 from data.etl import load_restaurant_data
 load_restaurant_data('./data/inspections.csv')
 
